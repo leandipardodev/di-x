2b098352918dbc3b894ba2aad0f7032470be230f
leandipardo <leandipardo@gmail.com>
2026-08-25 15:34:41 -0300

    fix: trigger ClipReveal 500px before cards enter viewport

--- src/components/Animations.tsx
+++ src/components/Animations.tsx
@@ -114,7 +114,7 @@ export function ClipReveal({
       <motion.div
         initial={{ y: "100%" }}
         whileInView={{ y: "0%" }}
-        viewport={{ once: true, margin: "-10%" }}
+        viewport={{ once: true, margin: "0px 500px 0px 0px" }}
         transition={{
           duration: 0.9,
           delay,
